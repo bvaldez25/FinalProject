2877f07d1154517f427b82c41706e615073328ca
bvaldez25 <bvaldez25@mail.fresnostate.edu>
2020-12-16 18:48:21 -0800

    finsihed structured-english

---
+++ final_project.py
@@ -0,0 +1,20 @@
+""""
+The Program is trying to determine which zodiac sign the user is.
+Based on the input the user enters, it will determine the zodiac sign.
+The user will first enter the day they were born.
+Second the user will enter the month they were born.
+Finally based on those inputs, the program will determine what zodiac sign the user is.
+
+If the user is born in December the output will display Sagittarius if the day is before 22 else output will be Capricorn.
+If the user is born in January the output will display Capricorn if the day is before 20 else output will be Aquarius.
+If the user is born in February the output will display Aquarius if the day is before 19 else output will be Pisces.
+If the user is born in March the output will display Pisces if the day is before 21 else output will be Aries.
+If the user is born in April the output will display Aries if the day is before 20 else output will be Taurus.
+If the user is born in May the output will display Taurus if the day is before 21 else output will be Gemini.
+If the user is born in June the output will display Gemini if the day is before 21 else output will be Cancer.
+If the user is born in July the output will display Cancer if the day is before 23 else output will be Leo.
+If the user is born in August the ouput will display Leo if the day is before 23 else output will be Virgo.
+If the user is born in September the output will display Virgo if the day is before 23 else output will be Libra.
+If the user is born in October the output will display Libra if the day is before 23 else the output will be Scorpio.
+If the user is born in November the output will display Scorpio if the day is before 22 else the output will be Sagittarius. 
+"""
